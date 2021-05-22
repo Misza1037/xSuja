ee1df3ff96c108a5de31fe8f54397530acd48805
Misza1037 <46529732+Misza1037@users.noreply.github.com>
2021-05-22 12:49:16 +0200

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -1,4 +1,4 @@
-def decompose(n):
+def dc(n):
   k = n
   r = []
   while k != 1:
@@ -6,10 +6,31 @@ def decompose(n):
     while k%i!=0:
       i+=1
     r.append(i)
-    
+    k = k//i
+  return r if len(r) != 0 else [1]
+
+
+def common(a,b): 
+  c = [value for value in a if value in b] 
+  return c
+
 
 class Fraction:
   def __init__(self, l):
     if type(l) != list: raise TypeError('')
     if len(l) != 2: raise ValueError('')
     self.l = l
+  def cut(self):
+    if self.l[0]//self.l[1] == self.l[0]/self.l[1]:
+      self.l[0] = self.l[0]//self.l[1]
+      self.l[1] = 1
+      return self.l
+    n = common(dc(self.l[0]), dc(self.l[1]))
+    if n == []: return self.l 
+    m = 1
+    for i in n:
+      m *= i
+    self.l[0] /= m
+    self.l[1] /= m
+    return self.l
+      
