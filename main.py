e029b27227a63eb910c0a245e2f59ae7026fbf02
sujemuje <84621465+sujemuje@users.noreply.github.com>
2021-05-22 12:00:36 +0200

    dodalem f(self, x) es

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ class Polynomial:
     self.args = args
   def __add__(self, other):
     return Polynomial(list(map(sum()), zip(self.args, other.args)))
-  #with comment
+  
+  def f(self, x):
+        sum = 0
+        for a in range(len(self.args)):
+            sum += x**a * self.args[a]
+        return sum
+
+a = Polynomial([0, 0, 1])
