a45f1b30e033de0792dbb056f15afbb48a394827
sujemuje <84621465+sujemuje@users.noreply.github.com>
2021-05-22 12:34:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 class Polynomial:
-  def __init__(self, *args):
-    self.args = args
-    self.p = self.args[0]
-    self.q = self.args[-1]
-    self.p_tab = self.zwroc_dzielniki(self.p)
-    self.q_tab = self.zwroc_dzielniki(self.q)
-    self.potencjalne_pierwiastki = self.zwroc_ilorazy(self.p_tab, self.q_tab)
-    self.pierwiastki = self.zwroc_pierwiastki(self.potencjalne_pierwiastki)
-    
-  def zwroc_ilorazy(self, P, Q):
-    tab = []
-    for p, q in zip(P, Q):
-      tab.append(p/q)
-    return tab
-  
-  def zwroc_dzielniki(self, a):
-    tab = []
-    for i in range(1, a//2 + 1):
-      if a % i == 0: tab.append(i)
-    return tab
-  
-  def zwroc_pierwiastki(self, a):
-    r = []
-    for x in a:
-      if f(x) == 0: r.append(x)
-    return r
-  
-  def f(self, x):
+    def __init__(self, args):
+        self.args = args
+        self.p = self.args[0]
+        self.q = self.args[-1]
+        self.p_tab = self.zwroc_dzielniki(self.p)
+        self.q_tab = self.zwroc_dzielniki(self.q)
+        self.potencjalne_pierwiastki = self.zwroc_ilorazy(self.p_tab, self.q_tab)
+        self.pierwiastki = self.zwroc_pierwiastki(self.potencjalne_pierwiastki)
+
+    def zwroc_ilorazy(self, P, Q):
+        tab = []
+        for p, q in zip(P, Q):
+            tab.append(p / q)
+        return tab
+
+    def zwroc_dzielniki(self, a):
+        tab = []
+        for i in range(1, a // 2 + 1):
+            if a % i == 0: tab.append(i)
+            print("lmao")
+        return tab
+
+    def zwroc_pierwiastki(self, a):
+        tab = []
+        for x in a:
+            if self.f(x) == 0:
+                tab.append(x)
+        return tab
+
+    def f(self, x):
         sum = 0
         for a in range(len(self.args)):
-            sum += x**a * self.args[a]
+            print(self.args)
+            sum += x ** a * self.args[a]
         return sum
 
-a = Polynomial([0, 0, 1])
+
+a = Polynomial([-81, 0, 0, 0, 1])
+print(a.p_tab)
+print(a.pierwiastki)
